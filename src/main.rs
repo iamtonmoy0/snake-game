ad1ad8a396ba9a2b3b3a85a24ce2a2e8430bb32d
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-25 20:11:32 -0400

    main.rs added

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,26 @@
  mod game;
  
  fn main() {
-   
+    let (width, height) = (30, 30);
+
+    let mut window: PistonWindow =
+        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
+
+    let mut game = Game::new(width, height);
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+        window.draw_2d(&event, |c, g, _| {
+            clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+    }
 }
