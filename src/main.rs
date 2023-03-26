8b68f5d2f5dddeccf56271480f035ba695ecae88
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-25 20:12:27 -0400

    error fixed

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@
  mod draw;
  mod snake;
  mod game;
- 
+ use piston_window::*;
+use piston_window::types::Color;
+
+use crate::game::Game;
+use crate::draw::to_coord_u32;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
  fn main() {
     let (width, height) = (30, 30);
 
